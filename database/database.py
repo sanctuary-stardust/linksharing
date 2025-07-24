ea29c2fc8e5f5d4415a4d5ffcf38bc6b743c841b
sanctuary-stardust <mangadownload6@gmail.com>
2025-07-24 12:33:36 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -1,3 +1,21 @@
+# database/database.py
+
+# For broadcasting or post management (e.g. newpost.py)
+channels = set()
+
+def save_channel(chat_id: str):
+    channels.add(chat_id)
+    return True
+
+def delete_channel(chat_id: str):
+    channels.discard(chat_id)
+    return True
+
+def get_channels():
+    return list(channels)
+
+
+# For forced subscription (multi-channel)
 forced_channels = set()
 
 def add_forced_channel(channel_id: str):
